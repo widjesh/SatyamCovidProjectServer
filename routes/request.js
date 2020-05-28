ce9209ad86d6b7af93b737aad13846df605ff299
widjesh <widjesh@satyamholidays.com>
2020-05-28 09:00:12 -0400

    Added formatation in mail

--- routes/request.js
+++ routes/request.js
@@ -53,26 +53,26 @@ router.post('/',async(req,res,next)=>{
                 }
               });
             
+              
               // send mail with defined transport object
               let info = await transporter.sendMail({
                 from: '"Satyam Holidays Covid-Team" <covid@satyamholidays.net>', // sender address
                 to: `${savedClient.email}`, // list of receivers
                 subject: "Submitted Data on Repatriating Form ✔", // Subject line
-                text:`
-                Dear ${savedClient.firstName} ${savedClient.lastName},
-                Beste reiziger  ,
-                Uw registratieaanvraag is ontvangen en zoals toegezegd doorgeleid naar de instanties 
-                Als U een ticket moet aanschaffen ( klanten met SLM of TUI ticket) graag na bevestiging kontakt met ons maken per mail op satyam@satyamholidays.net, zodat wij U de betalingsgegevens kunnen toezenden en na betaling, Uw ticket. 
+                html:`<p> Beste ${savedClient.firstName} ${savedClient.lastName},</br>
+                </br>
+                Uw registratieaanvraag is ontvangen en zoals toegezegd doorgeleid naar de instanties</br>
+                Als U een ticket moet aanschaffen ( klanten met SLM of TUI ticket) graag na bevestiging kontakt met ons maken per mail op satyam@satyamholidays.net,zodat wij U de betalingsgegevens kunnen toezenden en na betaling, Uw ticket. </br>
                 Voor appen en bellen zie onderaan dit bericht 
-                Het is ook mogelijk dat U zelf rechtstreeks  door KLM of de Ambassade benaderd wordt, echter staan wij in samenspraak met de instanties anytime klaar uw ticket makkelijk en snel te verzorgen of te wijzigen zodat u kunt afreizen
-                Wij spreken met u de hoop uit dat u gauw thuis bent
-                Satyam Holidays is telefonisch en per app  alsvolgt bereikbaar : 
-                In Nederland op het nummer 070-7113625 v.a 12:00  t/m 20:00 uur en via whats app op nr.+31707113625.
-                In Suriname op het nummer  1611   v.a  11:00  t/m 16:00  uur en  via whats app nr. +597436950
-                mvg
-                Satyam team 
-                Be safe
-                `
+                Het is ook mogelijk dat U zelf rechtstreeks  door KLM of de Ambassade benaderd wordt, echter staan wij in samenspraak met de instanties anytime klaar uw ticket makkelijk en snel te verzorgen of te wijzigen zodat u kunt afreizen </br>
+                Wij spreken met u de hoop uit dat u gauw thuis bent</br>
+                Satyam Holidays is telefonisch en per app  alsvolgt bereikbaar : </br>
+                In Nederland op het nummer 070-7113625 v.a 12:00  t/m 20:00 uur en via whats app op nr.+31707113625.</br>
+                In Suriname op het nummer  1611   v.a  11:00  t/m 16:00  uur en  via whats app nr. +597436950</br>
+                </br>
+                Mvg,</br>
+                Satyam team </br>
+                Be safe </p>`
               });
             
               console.log(`Message sent for ${savedClient.firstName} ${savedClient.lastName} : email = ${savedClient.email}`);
